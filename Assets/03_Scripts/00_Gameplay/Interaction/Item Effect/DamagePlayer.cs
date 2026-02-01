c8397f1487c8575826179baad8c993db2545ebf0
Ramaditya Firdaus <ramadityafirdaus11@gmail.com>
2026-02-01 12:29:05 +0700

    Fix Dice Damage

--- Assets/03_Scripts/00_Gameplay/Interaction/Item Effect/DamagePlayer.cs
+++ Assets/03_Scripts/00_Gameplay/Interaction/Item Effect/DamagePlayer.cs
@@ -8,6 +8,13 @@ public class DamagePlayer : ItemInteractEffect
 
     public override void ItemInteract(Character character)
     {
+        float calculate = character.CurrentHealth - damageValue;
+
+        if(calculate <= 0)
+        {
+            return;
+        }
+
         character.TakeDamage(damageValue);
         Debug.Log("Player Take Damage");
     }
